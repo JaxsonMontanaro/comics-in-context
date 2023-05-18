2f767bf6739b99a9976229d9b72ae7a431bb6c7d
JaxsonMontanaro <jaxsonmontanaro@gmail.com>
2023-05-17 21:45:40 -0700

    Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import StyleContext from './StyleContext';
+import UserInterface from './UserInterface';
 
-import UserInterface from "./UserInterface"
 
 function App() {
+  const styling = {
+  background: "#1ECD97",
+  display: "inline-block",
+  width: 120,
+  height: 55,
+  fontSize: 18,
+  letterSpacing: 1,
+  border: "2px solid #1ECD97",
+  borderRadius: 15
+};
+
   return (
     <main>
+      <StyleContext.Provider value={ styling }>
      <h1
           style={{
             color: "#FEE001",
@@ -16,6 +29,7 @@ function App() {
           Welcome to Comics Galore!
         </h1>
         <UserInterface />
+        </StyleContext.Provider>
     </main>
   );
 }
--- src/CollectorButton.jsx
+++ src/CollectorButton.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
+import StyleContext from "./StyleContext";
 
 
 export default function CollectorButton() {
-  
+  const buttonStyle  = useContext(StyleContext);
   return (
     <div>
-      <button>
+      <button style = {buttonStyle}>
         Click Here!
       </button>
     </div>
